8d3151e175daf62517b8ec706e688a9e77d59fa9
Rafał Maksim <rafalx117@gmail.com>
2017-04-14 22:51:39 +0200

    Merge branch 'master' of https://github.com/rafalx117/androidDruga
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- app/src/main/java/rafal/maksim/druga/MainActivity.java
+++ app/src/main/java/rafal/maksim/druga/MainActivity.java
@@ -1,13 +1,75 @@
 package rafal.maksim.druga;
 
+import android.app.ListActivity;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    SimpleCursorAdapter simpleCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+//        String query = "SELECT * FROM clients ORDER BY company_name ASC"; // No trailing ';'
+//        Cursor cursor = sqLiteDatabase.rawQuery(query, null);
+
+
+        String [] mapFrom = new String[]
+                {
+                        DBHelper.COLUMN_MAKE,
+                        DBHelper.COLUMN_MODEL,
+                        DBHelper.COLUMN_WWW
+                };
+
+        int [] mapTo = new int[]
+                {
+                        R.id.makeLabel,
+                        R.id.modelLabel,
+                        R.id.wwwLabel
+                };
+
+
+        DBHelper dbHelper = new DBHelper(this);
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+
+        //wyświetlenie wyników zapytania na które wskazuje cursor
+        simpleCursorAdapter = new SimpleCursorAdapter(this, R.layout.db_row, null, mapFrom, mapTo, 0);
+        setListAdapter(simpleCursorAdapter);
+
+
+
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String [] projection =
+                {
+                        DBHelper.ID,
+                        DBHelper.COLUMN_MAKE,
+                        DBHelper.COLUMN_MODEL
+                };
+        CursorLoader cursorLoader = new CursorLoader(this,Provider.URI_CONTENT,projection,null,null,null);
+        return cursorLoader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        simpleCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
