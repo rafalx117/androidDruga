da8310a191f5eb7cc92eedada2ed134dde9bc5f7
Rafał Maksim <rafalx117@gmail.com>
2017-04-16 17:32:24 +0200

    Autogeneracja adresu przy dodawaniu nowego telefonu + Przejście do strony WWW

--- app/src/main/java/rafal/maksim/druga/AddPhoneActivity.java
+++ app/src/main/java/rafal/maksim/druga/AddPhoneActivity.java
@@ -5,20 +5,103 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 
 import java.util.Random;
 
 public class AddPhoneActivity extends AppCompatActivity
 {
 
+    EditText makeEditText ;
+    EditText modelEditText;
+    EditText websiteEditText;
+    Switch autogenerateWebsiteSwitch;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_phone);
+        makeEditText = ((EditText)findViewById(R.id.makeAddLabel));
+        modelEditText =  ((EditText)findViewById(R.id.modelAddLabel));
+        websiteEditText = ((EditText)findViewById(R.id.websiteAddLabel));
+        autogenerateWebsiteSwitch = ((Switch)findViewById(R.id.autogenerateWebsiteSwitch) );
 
+        autogenerateWebsiteSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b)
+            {
+                if(((Switch)findViewById(R.id.autogenerateWebsiteSwitch)).isChecked() //metoda wygeneruje stronę internetową, jeśli zaznaczymy switch który był wcześniej wyłączony
+                        && !makeEditText.getText().toString().isEmpty()
+                        && !modelEditText.getText().toString().isEmpty()) //jeśli zaznaczona jest opccja autogeneracji adresu - generujemy adres
+                {
+                    websiteEditText.setText(generateWWWAddress());
+                }
+
+            }
+        });
+
+        makeEditText.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+                if(((Switch)findViewById(R.id.autogenerateWebsiteSwitch)).isChecked())
+                    websiteEditText.setText(generateWWWAddress());
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+
+            }
+        });
+
+        modelEditText.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+                if(((Switch)findViewById(R.id.autogenerateWebsiteSwitch)).isChecked())
+                    websiteEditText.setText(generateWWWAddress());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+
+            }
+        });
+
+    }
+
+    //metoda generująca link do wyszukiwania danego telefonu w google
+    public  String generateWWWAddress()
+    {
+        String make = ((EditText)findViewById(R.id.makeAddLabel)).getText().toString();
+        String model = ((EditText)findViewById(R.id.modelAddLabel)).getText().toString();
+        return "https://www.google.pl/search?&q="+make+"+"+ model;
     }
 
     public void savePhone(View view)
@@ -28,7 +111,7 @@ public class AddPhoneActivity extends AppCompatActivity
         ContentValues values = new ContentValues();
 
         String make = ((EditText)findViewById(R.id.makeAddLabel)).getText().toString();
-        String model = ((EditText)findViewById(R.id.makeAddLabel)).getText().toString();
+        String model = ((EditText)findViewById(R.id.modelAddLabel)).getText().toString();
         String website = ((EditText)findViewById(R.id.websiteAddLabel)).getText().toString();
 
         values.put(dbHelper.COLUMN_MODEL, model);
--- app/src/main/java/rafal/maksim/druga/EditPhoneActivity.java
+++ app/src/main/java/rafal/maksim/druga/EditPhoneActivity.java
@@ -1,24 +1,32 @@
 package rafal.maksim.druga;
 
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class EditPhoneActivity extends AppCompatActivity {
+
+    EditText modelEditText, makeEditText, websiteEditText;
+    int currentPhoneId = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_phone);
 
-        int currentPhoneId = -1;
-        String make = null;
-        String model = null;
-        String website = null;
+        makeEditText = ((EditText)findViewById(R.id.makeEditLabel));
+        modelEditText = ((EditText)findViewById(R.id.modelEditLabel));
+        websiteEditText = ((EditText)findViewById(R.id.websiteEditLabel));
+
+
         try
         {
             Intent intent = getIntent();
@@ -40,10 +48,43 @@ public class EditPhoneActivity extends AppCompatActivity {
         SQLiteDatabase database = dbHelper.getReadableDatabase(); //baza w trybie tylko do odczytu
         Cursor cursor = getContentResolver().query(Provider.URI_CONTENT, new String[]{dbHelper.COLUMN_MAKE, dbHelper.COLUMN_MODEL, dbHelper.COLUMN_WWW}, "_id = " + currentPhoneId, null, null);
         cursor.moveToFirst();
-        ((EditText)findViewById(R.id.makeEditLabel)).setText(cursor.getString(cursor.getColumnIndexOrThrow(dbHelper.COLUMN_MAKE)));
-        ((EditText)findViewById(R.id.modelEditLabel)).setText(cursor.getString(cursor.getColumnIndexOrThrow(dbHelper.COLUMN_MODEL)));
-
+        makeEditText.setText(cursor.getString(cursor.getColumnIndexOrThrow(dbHelper.COLUMN_MAKE)));
+        modelEditText.setText(cursor.getString(cursor.getColumnIndexOrThrow(dbHelper.COLUMN_MODEL)));
+        websiteEditText.setText(cursor.getString(cursor.getColumnIndexOrThrow(dbHelper.COLUMN_WWW)));
         database.close();
 
+    }
+
+    public void saveChanges(View view)
+    {
+        DBHelper dbHelper = new DBHelper(this);
+        SQLiteDatabase database = dbHelper.getWritableDatabase(); //metoda getWritableDatabase zwraca obiekt bazy, którą można edytować
+        ContentValues values = new ContentValues();
+
+        String make = ((EditText)findViewById(R.id.makeEditLabel)).getText().toString();
+        String model = ((EditText)findViewById(R.id.modelEditLabel)).getText().toString();
+        String website = ((EditText)findViewById(R.id.websiteEditLabel)).getText().toString();
+
+        values.put(dbHelper.COLUMN_MODEL, model);
+        values.put(dbHelper.COLUMN_MAKE,make);
+        values.put(dbHelper.COLUMN_WWW, website);
+//        Provider provider = new Provider();
+//        provider.update(Provider.URI_CONTENT,values,null,null);
+        //getContentResolver().update(ContentUris.withAppendedId(Provider.URI_CONTENT, currentPhoneId),values,null,null); //Karol
+        database.update(dbHelper.TABLE_NAME, values, dbHelper.ID + " = " + currentPhoneId, null);
+        database.close();
+
+        Intent intent = new Intent(this, MainActivity.class); //wracamy do strony głównej
+        startActivity(intent);
+    }
+
+    public void openBrowser(View view)
+    {
+        String url = websiteEditText.getText().toString();
+        if (!url.startsWith("http://") && !url.startsWith("https://"))
+            url = "http://" + url;
+
+        Intent intent = new Intent("android.intent.action.VIEW", Uri.parse(url));
+        startActivity(intent);
     }
 }
